df86dc1d7419a92bb0762189efdf264dec979cf5
ManuelBava <manuelbava1996@gmail.com>
2021-02-06 23:31:16 +0100

    Create RubricagalController.java

---
+++ rubrica-gal-server/src/main/java/it/sirfin/rubricagal/controller/RubricagalController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.sirfin.rubricagal.controller;
+
+import it.sirfin.rubricagal.dto.RubricaGal;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("*")
+@RestController
+public class RubricagalController {
+
+    private static int num;
+     List<RubricaGal> rubrica = new ArrayList<>();
+       
+       
+        
+
+}
