968d4702d1df5ea4601a7d6e169a94e014728dbf
ManuelBava <manuelbava1996@gmail.com>
2021-02-06 23:29:54 +0100

    Create RubricaGal.java

---
+++ rubrica-gal-server/src/main/java/it/sirfin/rubricagal/dto/RubricaGal.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.sirfin.rubricagal.dto;
+
+public class RubricaGal {
+
+    private int ID;
+    private String nome;
+    private String cognome;
+    private String telefono;
+
+    public RubricaGal() {
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
