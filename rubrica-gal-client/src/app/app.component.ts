fe2b6a5a0db6d9fa7727b7e2babef7da276b9c09
ManuelBava <manuelbava1996@gmail.com>
2021-02-07 22:44:34 +0100

    Update app.component.ts

--- rubrica-gal-client/src/app/app.component.ts
+++ rubrica-gal-client/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Contatto } from './contatto';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'rubrica-gal-client';
+  rubrica: Contatto[] = [];
+  nomeC: String;
+  cognomeC: String;
+  telefonoC: String;
+
+  constructor(private http: HttpClient) { }
+
+
+
+
+  inserisci() {
+    let dto = new Contatto();
+    dto.nome = this.nomeC;
+    dto.cognome = this.cognomeC;
+    dto.telefono = this.telefonoC;
+    let ox: Observable<Contatto[]> = this.http
+      .post<Contatto[]>("http://localhost:8080/rubrica",
+        dto);
+        ox.subscribe(r => this.rubrica = r)
+
+  }
+
+  rimuovi() {
+
+  }
+
 }
