586f2267fb57250f791cf9751726d4b3f2ef3cf3
ManuelBava <manuelbava1996@gmail.com>
2021-02-09 22:21:41 +0100

    viste conferma e annulla

--- rubrica-gal-client/src/app/app.component.ts
+++ rubrica-gal-client/src/app/app.component.ts
@@ -12,10 +12,10 @@ export class AppComponent {
   title(title: any) {
     throw new Error('Method not implemented.');
   }
-  
+
   // MODIFICA FATTA DA FEDERICO 08/02/21
   contatto: Contatto = new Contatto();
-  
+
   constructor(private http: HttpClient) { }
 
   //modifica leo 08/02/2020
@@ -32,24 +32,40 @@ export class AppComponent {
     let ox: Observable<Contatto> = this.http
       .post<Contatto>("http://localhost:8080/inserisciContatto",
         dto);
-        ox.subscribe(r => this.contatto = r)
-        this.stato="mod";
+    ox.subscribe(r => this.contatto = r)
+    this.stato = "mod";
 
   }
 
   rimuovi() {
-// Non funziona per adesso
-this.stato = "ins";
+    // Non funziona per adesso
+    this.stato = "con";
   }
 
-recupera(){
-  this.stato = "ins";
-}
+  recupera() {
+    this.stato = "con";
+  }
 
-cancella(){
-  
-  this.stato = "ins";
-}
+  cancella() {
+
+    this.stato = "con";
+  }
+
+  conferma() {
+
+    this.stato = "ins";
+  }
+
+  annulla() {
+
+    this.stato = "mod";
+  }
+
+  indietro() {
+
+    this.stato = "ins";
+  }
+
 
 
 }
