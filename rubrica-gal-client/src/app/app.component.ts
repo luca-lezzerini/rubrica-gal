14b55c48e74b4cd4c9666d216c044e610d84b916
leoIapp <leoiappelli01@gmail.com>
2021-02-14 22:56:15 +0100

    modifica viste "mod"

--- rubrica-gal-client/src/app/app.component.ts
+++ rubrica-gal-client/src/app/app.component.ts
@@ -43,7 +43,7 @@ export class AppComponent {
       .post<Contatto[]>("http://localhost:8080/eliminaContatti",
         dto);
     ox.subscribe(r => this.contatti= r)
-    this.stato = "ins";
+    
   }
 
   recupera() {
@@ -55,7 +55,7 @@ export class AppComponent {
       .post<Contatto[]>("http://localhost:8080/recuperaContatti",
         dto);
     ox.subscribe(r => this.contatti= r)
-    this.stato = "ins";
+    
 
   }
 
@@ -68,7 +68,7 @@ export class AppComponent {
       .post<Contatto[]>("http://localhost:8080/svuotaRubrica",
         dto);
     ox.subscribe(r => this.contatti= r)
-    this.stato = "ins";
+    
   }
 
   indietro() {
